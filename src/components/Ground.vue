f66acdfdd37b6a2f9bd7777ab9e52002cfe89c88
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-06-10 01:34:11 +0900

    refactor: component 분리

---
+++ src/components/Ground.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="ground">
+    <Header/>
+    <Boards/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Header from "./Header.vue";
+import Boards from "./Boards.vue";
+import Footer from "./Footer.vue";
+
+export default {
+  components: { Header, Boards, Footer },
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <header>Boards App</header>
+</template>
+
+<style scoped>
+header {
+    text-align: center;
+    padding:25px;
+    border-bottom: 1px solid #eee;
+    background: #f7f7f7;
+}
+</style>
