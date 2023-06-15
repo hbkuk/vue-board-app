c3dbcc8f027da7ca0e5a691c7491c57ec0dfe251
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-06-16 02:37:05 +0900

    refactor: App.vue 에서 마운트 전 실행되는 로직을 리팩토링

--- src/App.vue
+++ src/App.vue
@@ -5,40 +5,35 @@
 </template>
 
 <script>
+import {onBeforeMount} from 'vue';
+import store from '@/script/store';
 import Footer from "@/components/Footer.vue";
 import Header from "@/components/Header.vue";
-import {onBeforeMount} from "vue";
-import store from "@/script/store";
+import storeHelper from "@/script/storeHelper";
 
+/**
+ * 애플리케이션의 진입점인 App 컴포넌트입니다.
+ * Header, RouterView, Footer 컴포넌트를 렌더링합니다.
+ */
 export default {
   name: 'App',
-  components: {
-    Footer,
-    Header
-  },
+  components: {Header, Footer},
   setup() {
+    /**
+     * 컴포넌트가 마운트되기 전에 실행되는 로직을 처리합니다.
+     * sessionStorage에서 검색 조건을 로드하고, 해당 값을 스토어에 커밋합니다.
+     *
+     * @param {string} key - sessionStorage에서 로드할 값의 키
+     * @param {Function} commitFn - 스토어 커밋을 위한 함수
+     */
     onBeforeMount(() => {
-      // 컴포넌트가 마운트되기 전에 실행되는 로직
-      
-      const startDate = sessionStorage.getItem("startDate");
-      const endDate = sessionStorage.getItem("endDate");
-      const categoryIdx = sessionStorage.getItem("categoryIdx");
-      const keyword = sessionStorage.getItem("keyword");
-
-      if (startDate) {
-        store.commit('setStartDate', startDate);
-      }
-      if (endDate) {
-        store.commit('setEndDate', endDate);
-      }
-      if (categoryIdx) {
-        store.commit('setCategoryIdx', categoryIdx);
-      }
-      if(keyword) {
-        store.commit('setKeyword', keyword);
-      }
-
+      storeHelper.loadSearchCondition('startDate', (value) => store.commit('setStartDate', value));
+      storeHelper.loadSearchCondition('endDate', (value) => store.commit('setEndDate', value));
+      storeHelper.loadSearchCondition('categoryIdx', (value) => store.commit('setCategoryIdx', value));
+      storeHelper.loadSearchCondition('keyword', (value) => store.commit('setKeyword', value));
+      storeHelper.loadSearchCondition('pageNo', (value) => store.commit('setPageNo', value));
     });
   },
-}
+};
 </script>
+
