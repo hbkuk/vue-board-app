49ffa703f99fc0b1ab8f64c780458c518d88745e
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-06-15 18:27:33 +0900

    feat: 데이터 포맷팅 함수 추가

--- src/script/lib.js
+++ src/script/lib.js
@@ -3,10 +3,22 @@ export default {
     getCurrentDate() {
         return new Date().toISOString().slice(0, 10);
     },
+
     // 현재 날짜에서 일정 기간을 뺀 날짜를 계산하는 함수
     getPastDate(period) {
         const startDate = new Date();
         startDate.setDate(startDate.getDate() - period);
         return startDate.toISOString().slice(0, 10);
+    },
+
+    // "YYYY-MM-DDTHH:mm:ss" 형식의 날짜를 "YYYY.MM.DD HH:mm" 형식으로 변환
+    formatDate(dateString) {
+        const date = new Date(dateString);
+        const year = date.getFullYear();
+        const month = (date.getMonth() + 1).toString().padStart(2, '0');
+        const day = date.getDate().toString().padStart(2, '0');
+        const hours = date.getHours().toString().padStart(2, '0');
+        const minutes = date.getMinutes().toString().padStart(2, '0');
+        return `${year}-${month}-${day} ${hours}시${minutes}분`;
     }
 }
