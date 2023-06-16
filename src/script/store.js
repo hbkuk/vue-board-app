7859be6b6ff80f9e14182f5e2f59940d20e16f5d
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-06-16 15:55:07 +0900

    refactor: store에서 하나의 mutation을 사용하도록 리팩토링

--- src/script/store.js
+++ src/script/store.js
@@ -20,55 +20,9 @@ const store = createStore({
         }
     },
     mutations: {
-        /**
-         * 검색 시작 날짜를 설정합니다.
-         *
-         * @param {object} state - Vuex 상태 객체
-         * @param {string} payload - 시작 날짜
-         */
-        setStartDate(state, payload) {
-            state.searchCondition.startDate = payload;
-            sessionStorage.setItem("startDate", payload);
-        },
-        /**
-         * 검색 종료 날짜를 설정합니다.
-         *
-         * @param {object} state - Vuex 상태 객체
-         * @param {string} payload - 종료 날짜
-         */
-        setEndDate(state, payload) {
-            state.searchCondition.endDate = payload;
-            sessionStorage.setItem("endDate", payload);
-        },
-        /**
-         * 카테고리 인덱스를 설정합니다.
-         *
-         * @param {object} state - Vuex 상태 객체
-         * @param {string} payload - 카테고리 인덱스
-         */
-        setCategoryIdx(state, payload) {
-            state.searchCondition.categoryIdx = payload;
-            sessionStorage.setItem("categoryIdx", payload);
-        },
-        /**
-         * 키워드를 설정합니다.
-         *
-         * @param {object} state - Vuex 상태 객체
-         * @param {string} payload - 키워드
-         */
-        setKeyword(state, payload) {
-            state.searchCondition.keyword = payload;
-            sessionStorage.setItem("keyword", payload);
-        },
-        /**
-         * 페이지 번호를 설정합니다.
-         *
-         * @param {object} state - Vuex 상태 객체
-         * @param {string} payload - 페이지 번호
-         */
-        setPageNo(state, payload) {
-            state.searchCondition.pageNo = payload;
-            sessionStorage.setItem("pageNo", payload);
+        updateSearchCondition(state, payload) {
+            state.searchCondition = payload;
+            sessionStorage.setItem("searchCondition", JSON.stringify(payload));
         },
         /**
          * 게시물 목록을 설정합니다.
@@ -98,27 +52,6 @@ const store = createStore({
             state.categories = payload;
         }
     },
-    getters: {
-        /**
-         * 상태에서 게시판 목록을 가져옵니다.
-         *
-         * @param {object} state - Vuex 상태 객체입니다.
-         * @returns {Array} - 게시판 배열입니다.
-         */
-        getBoards: state => {
-            return state.boards;
-        },
-        /**
-         * 상태에서 카테고리 목록을 가져옵니다.
-         *
-         * @param {object} state - Vuex 상태 객체입니다.
-         * @returns {Array} - 카테고리 배열입니다.
-         */
-        getCategories: state => {
-            return state.categories;
-        },
-    },
-
 });
 
 export default store;
