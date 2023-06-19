ff6411d3cd292b79fda4373b76ea5bf02e34a238
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-06-19 11:18:53 +0900

    docs: jsdoc 추가

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import {createApp} from 'vue';
+import App from './App.vue';
 import router from "@/router/router";
 import store from "@/script/store";
 
-createApp(App).use(store).use(router).mount('#app')
+/**
+ * Vue 애플리케이션을 생성하고, 라우터와 상태 관리(store)를 등록하여 마운트합니다.
+ */
+createApp(App)
+    .use(store)
+    .use(router)
+    .mount('#app');
