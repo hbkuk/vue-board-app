924cf10eb840f8bf2d1315b2a699645646560eb8
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-06-20 17:45:37 +0900

    fix: Pagination 컴포넌트를 컴포지션 API 형태로 변경, 검색어 emit 추가

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,64 +1,49 @@
+<script setup>
+import {computed, defineProps} from 'vue';
+
+const props = defineProps({
+  pagination: Object,
+});
+
+const filteredPages = computed(() => {
+  const range = 2; // 현재 페이지를 기준으로 표시할 양쪽 페이지 수
+  const currentPage = props.pagination.pageNo;
+  const maxPage = props.pagination.maxPage;
+  const startPage = Math.max(1, currentPage - range);
+  const endPage = Math.min(maxPage, currentPage + range);
+  return Array.from({ length: endPage - startPage + 1 }, (_, i) => startPage + i);
+});
+
+// 페이지 변경 이벤트 처리
+const changePage = (page) => {
+  if (page === 'prev') {
+    console.log( '이전' )
+  } else if (page === 'next') {
+    console.log( '다음' )
+  } else {
+    console.log( '그외' )
+  }
+};
+</script>
+
 <template>
   <div class="pos-relative d-flex justify-content-center mt-5">
     <ul class="pagination pagination-sm">
       <li :class="['page-item', { 'disabled': pagination.pageNo === 1 }]">
-        <a class="page-link border-0 mx-1 pg-font-sm" href="#" @click.prevent="change(1)">처음</a>
+        <a class="page-link border-0 mx-1 pg-font-sm" href="#" @click.prevent="changePage(1)">처음</a>
       </li>
       <li :class="['page-item', { 'disabled': pagination.pageNo === 1 }]">
-        <a class="page-link border-0 mx-1 pg-font-sm" href="#" @click.prevent="change('prev')">이전</a>
+        <a class="page-link border-0 mx-1 pg-font-sm" href="#" @click.prevent="changePage('prev')">이전</a>
       </li>
-      <li
-          v-for="page in filteredPages"
-          :class="['page-item', { 'active': page === pagination.pageNo }]"
-          :key="page"
-      >
-        <a class="page-link border-0 mx-1 pg-font-sm" href="#" @click.prevent="change(page)">{{ page }}</a>
+      <li v-for="page in filteredPages " :class="['page-item', { 'active': page === pagination.pageNo }]" :key="page">
+        <a class="page-link border-0 mx-1 pg-font-sm" href="#" @click.prevent="changePage(page)">{{ page }}</a>
       </li>
       <li :class="['page-item', { 'disabled': pagination.pageNo === pagination.maxPage }]">
-        <a class="page-link border-0 mx-1 pg-font-sm" href="#" @click.prevent="change('next')">다음</a>
+        <a class="page-link border-0 mx-1 pg-font-sm" href="#" @click.prevent="changePage('next')">다음</a>
       </li>
       <li :class="['page-item', { 'disabled': pagination.pageNo === pagination.maxPage }]">
-        <a class="page-link border-0 mx-1 pg-font-sm" href="#" @click.prevent="change(pagination.maxPage)">끝</a>
+        <a class="page-link border-0 mx-1 pg-font-sm" href="#" @click.prevent="changePage(pagination.maxPage)">끝</a>
       </li>
     </ul>
   </div>
 </template>
-
-<script>
-import store from "@/script/store";
-import DataService from "@/service/DataService";
-import router from "@/router/router";
-import {mapActions, mapState} from "vuex";
-import queryHelper from "@/script/queryHelper";
-
-export default {
-  name: 'Pagination',
-  computed: {
-    ...mapState(['pagination']),
-    filteredPages() {
-      const { startPage, endPage, maxPage } = this.pagination;
-      return Array.from({ length: Math.min(endPage, maxPage) - startPage + 1 }, (_, index) => startPage + index);
-    },
-  },
-  methods: {
-    ...mapActions(['updateSearchCondition', 'updateBoards', 'updatePagination']),
-    change(direction) {
-      const searchCondition = store.state.searchCondition;
-      searchCondition.pageNo = direction === "prev" ? this.pagination.pageNo - 1 : direction === "next" ? this.pagination.pageNo + 1 : direction;
-
-      const filteredQuery = queryHelper.filteredSearchConditionParams(searchCondition);
-
-      DataService.fetchBoards(filteredQuery)
-          .then((res) => {
-            this.$store.dispatch('updateSearchCondition', filteredQuery);
-            this.$store.dispatch('updateBoards', res.boards);
-            this.$store.dispatch('updatePagination', res.pagination);
-          })
-          .catch((error) => {
-            console.error("Failed to fetch boards:", error);
-            router.push("/error");
-          });
-    },
-  },
-};
-</script>
