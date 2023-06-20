6971ade4b83bee867a811b5599b1cd01c16d9d06
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-06-20 14:08:38 +0900

    fix: 구현 방식 변경으로 인해 store 내용 삭제

--- src/script/store.js
+++ src/script/store.js
@@ -1,71 +1,8 @@
 import {createStore} from 'vuex';
-import lib from "@/script/lib";
 
-/**
- * Vuex 스토어를 생성합니다.
- */
+
 const store = createStore({
-    state() {
-        return {
-            searchCondition: {
-                startDate: lib.getPastDate(365),
-                endDate: lib.getCurrentDate(),
-                categoryIdx: "0",
-                keyword: "",
-                pageNo: "1",
-            },
-            boards: [],
-            pagination: {},
-            categories: {},
-        }
-    },
-    mutations: {
-        updateSearchCondition(state, payload) {
-            state.searchCondition = payload;
-        },
-        /**
-         * 게시물 목록을 설정합니다.
-         *
-         * @param {object} state - Vuex 상태 객체
-         * @param {array} payload - 게시물 목록
-         */
-        updateBoards(state, payload) {
-            state.boards = payload;
-        },
-        /**
-         * 페이지네이션 정보를 설정합니다.
-         *
-         * @param {object} state - Vuex 상태 객체
-         * @param {object} payload - 페이지네이션 정보
-         */
-        updatePagination(state, payload) {
-            state.pagination = payload;
-        },
-        /**
-         * 카테고리 목록을 설정합니다.
-         *
-         * @param {object} state - Vuex 상태 객체
-         * @param {array} payload - 카테고리 목록
-         */
-        updateCategories(state, payload) {
-            state.categories = payload;
-        }
-    },
-    actions: {
-        updateSearchCondition: ({commit}, payload) => {
-            commit('updateSearchCondition', payload);
-            sessionStorage.setItem("searchCondition", JSON.stringify(payload));
-        },
-        updateBoards: ({commit}, payload) => {
-            commit('updateBoards', payload)
-        },
-        updatePagination: ({commit}, payload) => {
-            commit('updatePagination', payload)
-        },
-        updateCategories: ({commit}, payload) => {
-            commit('updateCategories', payload)
-        },
-    },
+    state() {}
 });
 
 export default store;
