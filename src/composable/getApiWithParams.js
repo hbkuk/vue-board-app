f1c4a9f145e4cde6ef3fb77002ab64bae319ce40
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-06-22 18:15:42 +0900

    feat: get 요청 성공 시 해당 파라미터를 sessionStorage에 추가

--- src/composable/getApiWithParams.js
+++ src/composable/getApiWithParams.js
@@ -1,10 +1,11 @@
 import { ref, unref, isRef, watchEffect } from 'vue'
 import axios from 'axios'
 import router from '@/router/router'
-import { useFilterParams } from '@/script/filterParams'
+import { useFilterParams } from '@/composable/filterParams'
 
 /**
  * GET 방식으로 데이터를 가져오는 컴포저블
+ *
  * @param {string} url - API 엔드포인트 URL
  * @param {string} routerPath - 라우터 네비게이션을 위한 경로
  * @param {Object} params - API 요청에 사용되는 반응형 파라미터 (선택적)
@@ -26,7 +27,7 @@ export function useGetApiWithParams(url, routerPath, params) {
             //axios => 200번대 응답만을 성공으로 처리
             const response = await axios.get(url, { params: useFilterParams(unref(params)) }) // axios 기반 GET 요청
             await router.push({ path: routerPath, query: useFilterParams(unref(params)) }) // 라우터 네비게이션을 위해 URL 변경
-            console.log(useFilterParams(unref(params)))
+            sessionStorage.setItem('condition', JSON.stringify(useFilterParams(unref(params)))) // TODO: startDate=2022-06-22&endDate=2023-06-22&pageNo=1&ddd=3 => 허용되지 않는 조건에 대한 처리
             data.value = response.data
         } catch (err) {
             error.value = err.response.data // 상태코드 200번외의 응답에 대한 처리
