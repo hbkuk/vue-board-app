456a5ab81aa2dfd47f4c8b13c139963c200dc6d5
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-06-22 16:32:20 +0900

    fix: jsdoc 수정, 파일 이름 변경

---
+++ src/composable/getApi.js
@@ -0,0 +1,42 @@
+import { ref, unref, isRef, watchEffect } from 'vue'
+import axios from 'axios'
+import router from '@/router/router'
+import { useFilterParams } from '@/script/filterParams'
+
+/**
+ * GET 방식으로 데이터를 가져오는 컴포저블
+ * @param {string} url - API 엔드포인트 URL
+ * @param {string} routerPath - 라우터 네비게이션을 위한 경로
+ * @param {ref=} params - API 요청에 사용되는 반응형 파라미터 (선택적)
+ * @returns {object} - 가져온 데이터와 에러를 포함하는 객체
+ */
+export function useGetApi(url, routerPath, params = null) {
+    const data = ref(null)
+    const error = ref(null)
+
+    /**
+     * 데이터를 가져오는 작업을 수행합니다.
+     * @async
+     */
+    async function fetchData() {
+        data.value = null
+        error.value = null
+
+        try {
+            //axios => 200번대 응답만을 성공으로 처리
+            const response = await axios.get(url, { params: useFilterParams(unref(params)) }) // axios 기반 GET 요청
+            await router.push({ path: routerPath, query: useFilterParams(unref(params)) }) // 라우터 네비게이션을 위해 URL 변경
+            data.value = response.data
+        } catch (err) {
+            error.value = err.response.data // 상태코드 200번외의 응답에 대한 처리
+        }
+    }
+
+    if (isRef(params)) {
+        watchEffect(fetchData) // params가 ref인 경우에만 감시
+    } else {
+        fetchData() // 아닌 경우에는 초기에 한 번
+    }
+
+    return { data, error }
+}
--- src/script/filterParams.js
+++ src/script/filterParams.js
@@ -1,12 +1,12 @@
 /**
  * null 값을 제외한 필터링된 검색 조건 반환
  *
- * @param {Object} searchCondition - 검색 조건 객체
- * @returns {Object} - 필터링된 검색 조건 객체
+ * @param {Object} parameter - 파라미터 객체
+ * @returns {Object} - 필터링된 파라미터 객체
  * @example
  *
- * // 예시 1: 검색 조건에서 null 값을 제외하여 필터링합니다.
- * const searchCondition = {
+ * // 예시 1: 파라미터 객체에서 null 값을 제외하여 필터링
+ * const parameter = {
  *   startDate: '2023-01-01',
  *   endDate: null,
  *   categoryIdx: '1',
@@ -14,10 +14,10 @@
  *   pageNo: 1
  * };
  *
- * const filteredParams = filteredSearchCondition(searchCondition);
+ * const filteredParams = filterParams(parameter);
  * filteredParams: { startDate: '2023-01-01', categoryIdx: '1', pageNo: 1 }
  */
-export function useFilterCondition(parameter) {
+export function useFilterParams(parameter) {
     return Object.entries(parameter || {})
         .filter(([key, value]) => value !== null)
         .reduce((params, [key, value]) => {
