9b9926d3c7a8106c3b4157f6f1be07f490778443
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-06-22 23:34:00 +0900

    fix: 헤더 navbar 링크 수정

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light text-black">
     <div class="container px-3 px-lg-5">
-      <router-link to="/" class="navbar-brand">메인</router-link>
+      <router-link to="/boards" class="navbar-brand">메인</router-link>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
               aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span
           class="navbar-toggler-icon"></span></button>
