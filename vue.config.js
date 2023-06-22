ae38f68e0272169bc3830073328a641ff2a0f203
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-06-22 23:32:52 +0900

    doc: 파일 바이너리 다운로드 구현을 위한 프록시 설정

--- vue.config.js
+++ vue.config.js
@@ -7,6 +7,10 @@ module.exports = defineConfig({
       '/api': {
         target: 'http://localhost:8080',
         changeOrigin: true,
+      },
+      '/download': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
       }
     }
   }
