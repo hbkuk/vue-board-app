aaa0ddd5fedcea1d0044d12531fb55afcbc1f045
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-06-23 11:25:39 +0900

    doc: / 요청시 boards로 리다이렉트 설정

--- src/router/router.js
+++ src/router/router.js
@@ -4,11 +4,13 @@ import Home from "@/page/Home.vue";
 import Board from "@/page/Board.vue";
 import UpdateBoard from "@/page/UpdateBoard.vue";
 import DeleteBoard from "@/page/DeleteBoard.vue";
+import WriteBoard from "@/page/WriteBoard.vue";
 
 const routes = [
-    {path: '/', component: Home},
+    { path: '/', redirect: '/boards' }, // 리다이렉트 설정
     {path: '/boards', component: Boards},
     {path: '/board/:boardIdx', component: Board},
+    {path: '/board/write', component: WriteBoard},
     {path: '/board/modify/:boardIdx', component: UpdateBoard},
     {path: '/board/delete/:boardIdx', component: DeleteBoard},
 ]
