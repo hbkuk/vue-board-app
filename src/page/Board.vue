6bf6240229e4bbdc387f3facb78e6836b0462765
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-06-28 22:03:02 +0900

    refactor: Eslint 들여쓰기 수정

--- src/page/Board.vue
+++ src/page/Board.vue
@@ -15,9 +15,11 @@ const boardData = ref(null)
 const boardError = ref(null)
 /** 게시글 정보를 가져올때 발생하는 에러를 담는 반응성 객체 */
 
-const props = defineProps({
-  /** 전달받은 속성 */
-  boardIdx: String,
+const props = defineProps({ /** 전달받은 속성 */
+  boardIdx: {
+    type: String,
+    default: '',
+  },
 });
 
 /**
@@ -86,52 +88,76 @@ getBoard()
 </script>
 
 <template>
-  <WelcomeBanner :title="`커뮤니티`"
-                 :subTitle="`다양한 사람을 만나고 ....`"/>
+  <WelcomeBanner
+    :title="`커뮤니티`"
+    :sub-title="`다양한 사람을 만나고 ....`"
+  />
 
   <!-- 조건부 렌더링 1: 서버 통신 success -->
   <template v-if="boardData !== null">
-    <BoardDetail :boardData="boardData">
+    <BoardDetail :board-data="boardData">
       <div class="d-flex justify-content-between">
-        <b-button @click="modalShow = true" class="btn btn-danger font-weight-bold btn-sm">게시글 삭제</b-button>
-        <router-link class="btn btn-secondary font-weight-bold btn-sm"
-                     :to="`/board/modify/${boardData.board.boardIdx}`">게시글 수정
+        <b-button
+          class="btn btn-danger font-weight-bold btn-sm"
+          @click="modalShow = true"
+        >
+          게시글 삭제
+        </b-button>
+        <router-link
+          class="btn btn-secondary font-weight-bold btn-sm"
+          :to="`/board/modify/${boardData.board.boardIdx}`"
+        >
+          게시글 수정
         </router-link>
       </div>
-      <Comment v-if="boardData" :comments="boardData.board.comments"/>
+      <Comment
+        v-if="boardData"
+        :comments="boardData.board.comments"
+      />
     </BoardDetail>
   </template>
 
   <!-- 조건부 렌더링 2: 서버 통신 fail -->
   <template v-else-if="boardError !== null">
-    <Error :error="boardError"/>
+    <Error :error="boardError" />
   </template>
 
   <!-- 조건부 렌더링 3: 서버 통신 delay -->
   <template v-else>
-    <Spinner msg="게시글 가져오는 중 ..."/>
+    <Spinner msg="게시글 가져오는 중 ..." />
   </template>
 
   <!-- 모달 -->
-  <b-modal id="deleteModal" ref="modal" title="게시글 삭제 요청"
-           v-model="modalShow"
-           @show="resetPassword"
-           @hidden="resetPassword"
-           @ok="handleOk"
-           cancel-title="취소하기"
-           ok-title="삭제하기">
+  <b-modal
+    id="deleteModal"
+    ref="modal"
+    v-model="modalShow"
+    title="게시글 삭제 요청"
+    cancel-title="취소하기"
+    ok-title="삭제하기"
+    @show="resetPassword"
+    @hidden="resetPassword"
+    @ok="handleOk"
+  >
     <form ref="form">
-      <b-form-group label="password" label-for="password-input" invalid-feedback="password is required">
+      <b-form-group
+        label="password"
+        label-for="password-input"
+        invalid-feedback="password is required"
+      >
         <b-form-input
-            type="password"
-            id="password-input"
-            v-model="password"
-            required
-        ></b-form-input>
+          id="password-input"
+          v-model="password"
+          type="password"
+          required
+        />
       </b-form-group>
     </form>
     <div v-if="submitError">
-      <div class="alert alert-danger text-center" role="alert">
+      <div
+        class="alert alert-danger text-center"
+        role="alert"
+      >
         {{ submitError.detail }}
       </div>
     </div>
