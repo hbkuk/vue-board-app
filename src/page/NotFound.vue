09b9ad91d45dcfcdffd4e914ccf80f42ac916a46
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-06-29 13:40:35 +0900

    feat: router에 선언된 path가 아닌 경우 NotFound 컴포넌트로 리다이렉트 기능

---
+++ src/page/NotFound.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="not-found">
+    <div class="status">
+      404
+    </div>
+    <div class="message">
+      Page Not Found!
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+.not-found {
+  line-height: 1.2;
+  text-align: center;
+  font-family: "Oswald", sans-serif;
+  padding: 80px 20px;
+}
+
+.not-found .status {
+  font-size: 160px;
+  color: $primary;
+}
+
+.not-found .message {
+  font-size: 50px;
+}
+
+</style>
--- src/router/router.js
+++ src/router/router.js
@@ -3,6 +3,7 @@ import Boards from "@/page/Boards.vue";
 import Board from "@/page/Board.vue";
 import WriteBoard from "@/page/WriteBoard.vue";
 import ModifyBoard from "@/page/ModifyBoard.vue";
+import NotFound from "@/page/NotFound.vue";
 
 const routes = [
     {path: '/', redirect: '/boards'}, // 리다이렉트 설정
@@ -10,6 +11,7 @@ const routes = [
     {path: '/board/:boardIdx', name: 'Board', component: Board, props: true},
     {path: '/board/write', name: 'WriteBoard', component: WriteBoard},
     {path: '/board/modify/:boardIdx', component: ModifyBoard, props: true},
+    {path: '/:catchAll(.*)*', name: 'NotFound', component: NotFound}, // 404 페이지로 리다이렉트
 ]
 
 const router = createRouter({
